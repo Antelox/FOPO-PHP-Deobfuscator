29e1c4dad5801f6cb815b1538407dca17870bc2b
Antelox <anteloxrce@gmail.com>
2017-05-04 17:18:29 +0200

    Replaced tabs with spaces

--- ver.-0.1/deobfuscator.py
+++ ver.-0.1/deobfuscator.py
@@ -42,7 +42,7 @@ import re
 
 def str_rot13(string):
     return string.encode('rot13')
-	
+    
 def base64_decode(string):
     return base64.b64decode(string)
 
@@ -51,49 +51,49 @@ def gzinflate(string):
 
 #main
 if len(sys.argv) > 1:
-	print "\n***FOPO Deobfuscator***\n"
-	
-	contents = open(sys.argv[1],'r').read()
-	if "Obfuscation provided by FOPO - Free Online PHP Obfuscator:" not in contents:
-		print "*ERROR: Provided a PHP script not obfuscated with FOPO PHP Obfuscator!"
-		sys.exit()
+    print "\n***FOPO Deobfuscator***\n"
+    
+    contents = open(sys.argv[1],'r').read()
+    if "Obfuscation provided by FOPO - Free Online PHP Obfuscator:" not in contents:
+        print "*ERROR: Provided a PHP script not obfuscated with FOPO PHP Obfuscator!"
+        sys.exit()
 
-	eval = contents.split('(')
-	
-	#base64 = base64 encoded block inside obfuscated PHP script
-	base64_ = eval[3].split('"')
-	
-	i1 = base64_decode(base64_[1]).split("eval")
-	
-	#there is a ternary operator at this point "?:" -> (condition) ? (expr for TRUE) : (expr 4 FALSE)
-	#the right data block to be decoded is the second one, that is the data block relative to ":" (FALSE)
-	i2 = i1[1].split(':')
-	i3 = i2[1].split('"')
-	
-	#initialization variables
-	encryptionlayer = ''
-	dl = ''
-	nextlayer = ''
-	backup = ''
-	#Here final steps with n recursive encoded layers: gzinflate(base64_decode(str_rot13(datablock)))
-	#First layer here
-	encryptionlayer = gzinflate(base64_decode(str_rot13(i3[1])))
-	
-	#n-1 remaining layers inside while loop below
-	while (str(re.match('function [a-z]{26}', encryptionlayer)) == 'None'):
-		backup = encryptionlayer
-		dl = encryptionlayer.split('("')
-		nextlayer = gzinflate(base64_decode(str_rot13(dl[1])))
-		encryptionlayer = nextlayer
-	
-	#here final[1] variable contains deobfuscated PHP code :D
-	final = backup.split('?>')
-	try:
-		open(sys.argv[2],'wb').write(final[1])
-	except:
-		open('deobfuscated.php','wb').write(final[1])
+    eval = contents.split('(')
+    
+    #base64 = base64 encoded block inside obfuscated PHP script
+    base64_ = eval[3].split('"')
+    
+    i1 = base64_decode(base64_[1]).split("eval")
+    
+    #there is a ternary operator at this point "?:" -> (condition) ? (expr for TRUE) : (expr 4 FALSE)
+    #the right data block to be decoded is the second one, that is the data block relative to ":" (FALSE)
+    i2 = i1[1].split(':')
+    i3 = i2[1].split('"')
+    
+    #initialization variables
+    encryptionlayer = ''
+    dl = ''
+    nextlayer = ''
+    backup = ''
+    #Here final steps with n recursive encoded layers: gzinflate(base64_decode(str_rot13(datablock)))
+    #First layer here
+    encryptionlayer = gzinflate(base64_decode(str_rot13(i3[1])))
+    
+    #n-1 remaining layers inside while loop below
+    while (str(re.match('function [a-z]{26}', encryptionlayer)) == 'None'):
+        backup = encryptionlayer
+        dl = encryptionlayer.split('("')
+        nextlayer = gzinflate(base64_decode(str_rot13(dl[1])))
+        encryptionlayer = nextlayer
+    
+    #here final[1] variable contains deobfuscated PHP code :D
+    final = backup.split('?>')
+    try:
+        open(sys.argv[2],'wb').write(final[1])
+    except:
+        open('deobfuscated.php','wb').write(final[1])
 
 else:
-	print "\n*ERROR: Please provide the input file name as argument!"
-	print "\nExample: python deobfuscator.py input.php [output=deobfuscated.php]"
-	sys.exit()
+    print "\n*ERROR: Please provide the input file name as argument!"
+    print "\nExample: python deobfuscator.py input.php [output=deobfuscated.php]"
+    sys.exit()
